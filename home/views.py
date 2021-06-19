db6c5691fcefe092274278cccff2b125b383c919
abd-kaya <abdawd1993@gmail.com>
2021-06-19 17:57:53 +0300

    project settings contact & about pages

--- home/views.py
+++ home/views.py
@@ -1,9 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from home.models import Setting
+from hotel.models import Category
+
 
 def index(request):
-    university="karabuk University"
-    dept="computer engineering"
-    context={'university':university,'department':dept}
-    return render(request,'index.html',context)
+    setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    page = "home"
+    context = {'setting': setting,
+               'page': page,
+               'category': category}
+    return render(request, 'index.html', context)
+
+
+def aboutus(request):
+    setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    context = {'setting': setting,
+               'category': category}
+    return render(request, 'aboutus.html', context)
+
+def contact(request):
+    setting = Setting.objects.get(pk=1)
+    category = Category.objects.all()
+    context = {'setting': setting,
+               'category': category}
+    return render(request, 'contact.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,14 +17,18 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
-
     path('', include('home.urls')),
     path('home/', include('home.urls')),
     path('hotel/', include('hotel.urls')),
-    path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
+    path('admin/', admin.site.urls),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('aboutus/', views.aboutus, name='aboutus'),
+    path('contact/', views.contact, name='contact'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
