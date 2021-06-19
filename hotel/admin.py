c88679c8f8c6817ce68f07ee789093f9f4523dfc
abd-kaya <abdawd1993@gmail.com>
2021-06-19 03:07:24 +0300

    image gallery

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from hotel.models import Category, Hotel
+from hotel.models import Category, Hotel, Images
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'parent', 'status', 'update_at']
     list_filter = ['status']
 
+class HotelImageInline(admin.TabularInline):
+    model = Images
+    extra = 3
+
 class HotelAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'status', 'update_at']
     list_filter = ['category']
+    readonly_fields = ('image_tag',)
+    inlines = [HotelImageInline]
 
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['image','title']
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Hotel,HotelAdmin)
+admin.site.register(Images,ImagesAdmin)
--- hotel/models.py
+++ hotel/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'True'),
@@ -37,6 +40,22 @@ class Hotel(models.Model):
     status = models.CharField(max_length=10,choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        if self.image.url is not None:
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+        else:
+            return ""
+
+    image_tag.short_description = 'Image'
+
+class Images(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
 
     def __str__(self):
         return self.title
