995431173dcd084c897d78457e9b62e472c75e41
abd-kaya <abdawd1993@gmail.com>
2021-06-19 20:48:46 +0300

    contact us form and messages

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-from home.models import Setting
+from django.contrib import messages
+from home.models import Setting, ContactForm, ContactMessage
 from hotel.models import Category
 
 
@@ -17,14 +17,25 @@ def index(request):
 
 def aboutus(request):
     setting = Setting.objects.get(pk=1)
-    category = Category.objects.all()
-    context = {'setting': setting,
-               'category': category}
+    context = {'setting': setting,}
     return render(request, 'aboutus.html', context)
 
 def contact(request):
+    if request.method == 'POST':  # check post
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            data = ContactMessage()  # create relation with model
+            data.name = form.cleaned_data['name']  # get form input data
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()  # save data to table
+            messages.success(request, "Your message has ben sent. Thank you for your message.")
+            return HttpResponseRedirect('/contact')
+
     setting = Setting.objects.get(pk=1)
-    category = Category.objects.all()
+    form = ContactForm
     context = {'setting': setting,
-               'category': category}
+               'form': form,}
     return render(request, 'contact.html', context)
